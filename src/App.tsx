383bd54a0af77a080815990b9fbd9318ccdfcb2c
Ellams Bliss <ellamsbliss@gmail.com>
2023-08-18 18:55:45 +1000

    setting up styled component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 
 // components
-import { Drawer, LinearProgress, Grid,Badge } from "@material-ui/core";
+import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+
+// styles
+import { Wrapper } from "./App.styles";
 
 const App = () => {
   return <div className="App">start</div>;
