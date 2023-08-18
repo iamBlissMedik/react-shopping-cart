3d64819d6585e6b1319e3edacc6b715db68a32f9
Ellams Bliss <ellamsbliss@gmail.com>
2023-08-19 01:17:44 +1000

    add to cart button

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { AddShoppingCart } from "@material-ui/icons";
 import { Wrapper, StyledButton } from "./App.styles";
 import { CartItemType } from "./types/CartItemType";
 import Item from "./components/Item/Item";
+import Cart from "./components/Cart/Cart";
 
 const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
@@ -22,14 +23,35 @@ const App = () => {
   );
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount, 0);
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // is item already added in the cart
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? {
+                ...item,
+                amount: item.amount + 1,
+              }
+            : item
+        );
+      }
+      // first time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
   const handleRemoveFromCart = () => null;
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>;
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        cart goes here
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
