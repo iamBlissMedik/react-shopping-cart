6dd04e83f31bec962124e3f0118fcce65e0bda68
Ellams Bliss <ellamsbliss@gmail.com>
2023-08-18 19:00:59 +1000

    getProducts function

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import { AddShoppingCart } from "@material-ui/icons";
 // styles
 import { Wrapper } from "./App.styles";
 
+const getProducts = async () => {
+  await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
 const App = () => {
   return <div className="App">start</div>;
 };
