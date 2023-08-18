6c015885585ba18bd3ae29ce662d5fc12a354760
Ellams Bliss <ellamsbliss@gmail.com>
2023-08-18 19:11:37 +1000

    types for product

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import { AddShoppingCart } from "@material-ui/icons";
 
 // styles
 import { Wrapper } from "./App.styles";
+import { CartItemType } from "./types/CartItemTypes";
 
-const getProducts = async () => {
+const getProducts = async (): Promise<CartItemType> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
-};
 
 const App = () => {
   return <div className="App">start</div>;
