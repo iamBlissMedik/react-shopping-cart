4b57ae6fbe0281dc02ed54337ca5dfad50125ec0
Ellams Bliss <ellamsbliss@gmail.com>
2023-08-18 19:45:02 +1000

    item container

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,33 @@ import { AddShoppingCart } from "@material-ui/icons";
 
 // styles
 import { Wrapper } from "./App.styles";
-import { CartItemType } from "./types/CartItemTypes";
+import { CartItemType } from "./types/CartItemType";
+import Item from "./components/Item/Item";
 
-const getProducts = async (): Promise<CartItemType> =>
+const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  return <div className="App">start</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
